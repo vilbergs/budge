003ff9025f41b59ee3be63df26dace32f4ab66f5
Vilberg Eliasson <vilbergs@gmail.com>
2022-04-19 08:13:10 +0200

    Better dashboard values

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -9,6 +9,8 @@ import {
   isWithinInterval,
   isThisYear,
   subDays,
+  startOfYear,
+  endOfYear,
   classNames,
 } from '~/utils'
 
@@ -63,8 +65,9 @@ const sumTransactionsByType = (
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request, '/dashboard')
-  const end = new Date()
-  const start = subYears(end, 1)
+  const today = new Date()
+  const end = endOfYear(today)
+  const start = startOfYear(today)
 
   const transactions = await getTransactionListItems({
     userId,
@@ -116,22 +119,29 @@ export const loader: LoaderFunction = async ({ request }) => {
     {}
   )
 
-  const today = new Date()
   const thirtyDaysFromToday = subDays(today, 30)
   const previousThirtyDays = subDays(thirtyDaysFromToday, 30)
 
-  const transactionsFromLastThirtyDays = transactions.filter((transaction) =>
-    isWithinInterval(new Date(transaction.date), {
-      start: thirtyDaysFromToday,
-      end: today,
-    })
+  const transactionsLastSixtyDays = await getTransactionListItems({
+    userId,
+    start: previousThirtyDays,
+    end: today,
+  })
+
+  const transactionsFromLastThirtyDays = transactionsLastSixtyDays.filter(
+    (transaction) =>
+      isWithinInterval(new Date(transaction.date), {
+        start: thirtyDaysFromToday,
+        end: today,
+      })
   )
 
-  const transactionsFromPreviusThirtyDays = transactions.filter((transaction) =>
-    isWithinInterval(new Date(transaction.date), {
-      start: previousThirtyDays,
-      end: thirtyDaysFromToday,
-    })
+  const transactionsFromPreviusThirtyDays = transactionsLastSixtyDays.filter(
+    (transaction) =>
+      isWithinInterval(new Date(transaction.date), {
+        start: previousThirtyDays,
+        end: thirtyDaysFromToday,
+      })
   )
 
   const calculateChange = (original: currency, newAmount: currency) => {
@@ -194,70 +204,78 @@ export default function Dashboard() {
   const { stats, transactionSumsByMonth } = useLoaderData() as LoaderData
 
   return (
-    <div className="space-y-4 p-4 sm:p-8">
-      <h3 className="text-lg font-medium leading-6 text-gray-900">
-        Last 30 days
-      </h3>
-      <dl className="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x">
-        {stats.map((item) => (
-          <div key={item.name} className="px-4 py-5 sm:p-6">
-            <dt className="text-base font-normal text-gray-900">{item.name}</dt>
-            <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
-              <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
-                {item.stat}
-                <span className="ml-2 text-sm font-medium text-gray-500">
-                  from {item.previousStat}
-                </span>
-              </div>
+    <div className="space-y-10 p-4 sm:p-8">
+      <div>
+        <h3 className="text-lg font-medium leading-6 text-gray-900">
+          Last 30 days
+        </h3>
+        <dl className="mt-4 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-y-0 md:divide-x">
+          {stats.map((item) => (
+            <div key={item.name} className="px-4 py-5 sm:p-6">
+              <dt className="text-base font-normal text-gray-900">
+                {item.name}
+              </dt>
+              <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
+                <div className="flex items-baseline text-2xl font-semibold text-indigo-600">
+                  {item.stat}
+                  <span className="ml-2 text-sm font-medium text-gray-500">
+                    from {item.previousStat}
+                  </span>
+                </div>
 
-              <div
-                className={classNames(
-                  item.changeType === 'increase'
-                    ? 'bg-green-100 text-green-800'
-                    : 'bg-red-100 text-red-800',
-                  'inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0'
-                )}
-              >
-                {item.changeType === 'increase' ? (
-                  <ArrowSmUpIcon
-                    className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
-                    aria-hidden="true"
-                  />
-                ) : (
-                  <ArrowSmDownIcon
-                    className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
-                    aria-hidden="true"
-                  />
-                )}
+                <div
+                  className={classNames(
+                    item.changeType === 'increase'
+                      ? 'bg-green-100 text-green-800'
+                      : 'bg-red-100 text-red-800',
+                    'inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0'
+                  )}
+                >
+                  {item.changeType === 'increase' ? (
+                    <ArrowSmUpIcon
+                      className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <ArrowSmDownIcon
+                      className="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-red-500"
+                      aria-hidden="true"
+                    />
+                  )}
 
-                <span className="sr-only">
-                  {item.changeType === 'increase' ? 'Increased' : 'Decreased'}{' '}
-                  by
-                </span>
-                {item.change}
-              </div>
-            </dd>
-          </div>
-        ))}
-      </dl>
-
-      <div className="overflow-hidden rounded-md bg-white shadow">
-        <ul className="divide-y divide-gray-200">
-          {Object.entries(transactionSumsByMonth).map(
-            ([month, { EXPENSE, INCOME, total }]) => (
-              <li
-                key={month}
-                className="flex items-center justify-between px-6 py-4"
-              >
-                <span>{month}</span>
-                <Amount amount={INCOME} />
-                <Amount amount={EXPENSE} />
-                {}
-                <Amount amount={total} />
-              </li>
-            )
-          )}
-        </ul>
+                  <span className="sr-only">
+                    {item.changeType === 'increase' ? 'Increased' : 'Decreased'}{' '}
+                    by
+                  </span>
+                  {item.change}
+                </div>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+      <div>
+        <h3 className="text-lg font-medium leading-6 text-gray-900">
+          {new Date().getFullYear()}
+        </h3>
+        <div className="mt-4 overflow-hidden rounded-md bg-white shadow">
+          <ul className="divide-y divide-gray-200">
+            {Object.entries(transactionSumsByMonth).map(
+              ([month, { EXPENSE, INCOME, total }]) => (
+                <li
+                  key={month}
+                  className="flex items-center justify-between px-6 py-4"
+                >
+                  <span>{month}</span>
+                  <Amount amount={INCOME} />
+                  <Amount amount={EXPENSE} />
+                  {}
+                  <Amount amount={total} />
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   )
